9b1953e0445a404672bb4027bac0e590b4d9f606
Sistemasupervision <cpnbtecnozulia@gmail.com>
2025-11-28 19:43:49 -0400

    Delete backend directory

--- backend/config.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = {
-  SUPABASE_URL: 'https://ksczzvtgncvxfrmtvmll.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzY3p6dnRnbmN2eGZybXR2bWxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3NjMyMzcsImV4cCI6MjA3NDMzOTIzN30.AuWa3uaRylpXhz_VUh097k5tgNHuDllN8j-Hrktwzno'
-};
