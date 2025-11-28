abc174c4cbb299b1442d2d17e799d2b89d08a34d
Sistemasupervision <cpnbtecnozulia@gmail.com>
2025-11-28 17:06:03 -0400

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,6 @@
-
+// assets/js/config.js
 const supabaseUrl = 'https://ksczzvtgncvxfrmtvmll.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzY3p6dnRnbmN2eGZybXR2bWxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3NjMyMzcsImV4cCI6MjA3NDMzOTIzN30.AuWa3uaRylpXhz_VUh097k5tgNHuDllN8j-Hrktwzno';
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// ✅ Usa el cliente global ya disponible
+const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
